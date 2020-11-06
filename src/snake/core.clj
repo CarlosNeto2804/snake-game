e4ae2c7e3a29c816ecf8374ed435880d22cc80af
Carlos Alberto Neto <carloshtcursos@gmail.com>
2020-11-06 19:59:23 -0400

    Adding dependences

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -1,3 +1,7 @@
-(ns snake.core)
+(ns snake.core
+  (:import (java.awt Color Dimension Font)
+           (javax.swing JPanel JFrame Timer JOptionPane)
+           (java.awt.event ActionListener KeyListener KeyEvent))
+  (:gen-class))
 (defn -main [& args]
   (println "hello clojure"))
